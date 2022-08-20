adaf2ea38369644e50a79cac0246d606f0c73fc7
Hackagon <phonghiavan@gmail.com>
2022-08-20 10:23:04 +0700

    pagination

--- src/database/user/provider/users.provider.ts
+++ src/database/user/provider/users.provider.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import { paginateRaw } from "src/common/pagination";
 import { Repository } from "typeorm";
 import { UserEntity } from "../user.entity";
 
@@ -10,6 +11,14 @@ export class UsersProvider {
   ) { }
 
   async findAll() {
-    return await this._repository.find();
+    // return await this._repository.find();
+    return paginateRaw(
+      this._repository.createQueryBuilder().select(),
+      {
+        limit: 2,
+        page: 4,
+        route: 'localhost:3000/api/users'
+      }
+    )
   }
 }
